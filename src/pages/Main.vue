8acfdb8d38697865fb6acf42dfb9a329b2be2a83
dsa25 <73094994+dsa25@users.noreply.github.com>
2023-07-27 21:47:06 +0500

    Update Main.vue

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -2,7 +2,7 @@
     <section class="page__content">
         <div class="row">
         <div class="page__head">
-            <h1>tasks page</h1>
+            <h1>main page</h1>
         </div>
         <p>go to <router-link to="/tasks">tasks</router-link></p>
         </div>
